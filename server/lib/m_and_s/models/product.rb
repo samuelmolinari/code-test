9e2658e9dc50c888be0aab861704c966a80c2533
Samuel Molinari <samuel@molinari.me>
2015-05-02 15:36:53 +0100

    Implement product attributes
    - name
    - code
    - price

--- server/lib/m_and_s/models/product.rb
+++ server/lib/m_and_s/models/product.rb
@@ -1,4 +1,14 @@
+require 'money'
+
 module MAndS
+  # M&S product class
   class Product
+    attr_accessor :name, :code, :price
+
+    def initialize(name: nil, code: nil, price: nil)
+      self.name = name
+      self.code = code
+      self.price = price
+    end
   end
 end
--- server/spec/m_and_s/models/product_spec.rb
+++ server/spec/m_and_s/models/product_spec.rb
@@ -1,4 +1,20 @@
 require 'spec_helper'
 
 describe ::MAndS::Product do
+  describe 'product creation' do
+    it 'can be initialised with a name' do
+      name = 'Product Name'
+      MAndS::Product.new(name: name).name.must_equal name
+    end
+
+    it 'can be initialised with a code' do
+      code = 'CODE1234'
+      MAndS::Product.new(code: code).code.must_equal code
+    end
+
+    it 'can be initialised with a price' do
+      price = Money.new(1000, 'GBP')
+      MAndS::Product.new(price: price).price.must_equal price
+    end
+  end
 end
