3358419ed6c6ebdebd87f4cb31ed4892fc8dbdb9
Samuel Molinari <samuel@molinari.me>
2015-05-03 12:11:44 +0100

    Add test coverage
    - Add simplecov as dependency
    - Create rake task test
    
    Known issues:
    - simplecov does not work well with guard, use rake task instead to
    generate test coverage
    
    TODO:
    - Add environment for test coverage only, to improve guard efficiency

--- server/spec/spec_helper.rb
+++ server/spec/spec_helper.rb
@@ -1,8 +1,9 @@
-ENV['RACK_ENV'] = 'test'
+require 'simplecov'
+SimpleCov.start
 
 require 'minitest/autorun'
-require_relative 'support/factory_girl'
 require_relative '../lib/m_and_s'
+require_relative 'support/factory_girl'
 
 FactoryGirl.definition_file_paths = [File.expand_path('../factories', __FILE__)]
 FactoryGirl.find_definitions
